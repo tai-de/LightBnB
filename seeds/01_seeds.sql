6c1430ea14e2a2f49687ef270ad54e3eace58701
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2022-12-13 19:38:25 -0700

    create initial seed files, 3 entries to each table

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,109 @@
+INSERT INTO
+  users (name, email, PASSWORD)
+VALUES
+  (
+    'Tai Delisle',
+    'tai@test.com',
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
+  ),
+  (
+    'Tony Stark',
+    'tony@stark.com',
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
+  ),
+  (
+    'Harry Potter',
+    'chosen@one.com',
+    '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'
+  );
+
+INSERT INTO
+  properties (
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+    country,
+    street,
+    city,
+    province,
+    post_code,
+    active
+  )
+VALUES
+  (
+    1,
+    'Casa',
+    'description',
+    'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350',
+    'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg',
+    930.61,
+    6,
+    4,
+    8,
+    'Canada',
+    '1234 A Highway',
+    'Region',
+    'Alberta',
+    'T0M 4T0',
+    TRUE
+  ),
+  (
+    1,
+    'Blank corner',
+    'description',
+    'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350',
+    'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg',
+    852.34,
+    6,
+    6,
+    7,
+    'Canada',
+    '651 Nami Road',
+    'Bohbatev',
+    'Alberta',
+    'T3W0M0',
+    TRUE
+  ),
+  (
+    2,
+    'Habit mix',
+    'description',
+    'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350',
+    'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg',
+    460.58,
+    0,
+    5,
+    6,
+    'Canada',
+    '1650 Hejto Center',
+    'Genwezuj',
+    'Newfoundland and Labrador',
+    'N0M0M0',
+    TRUE
+  );
+
+INSERT INTO
+  reservations (start_date, end_date, property_id, guest_id)
+VALUES
+  ('2018-09-11', '2018-09-26', 2, 3),
+  ('2019-01-04', '2019-02-01', 2, 2),
+  ('2023-10-01', '2023-10-14', 1, 3);
+
+INSERT INTO
+  property_reviews (
+    guest_id,
+    property_id,
+    reservation_id,
+    rating,
+    message
+  )
+VALUES
+  (3, 2, 1, 3, 'messages'),
+  (2, 2, 2, 4, 'messages'),
+  (3, 1, 3, 4, 'messages');
