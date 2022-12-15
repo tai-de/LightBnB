1b95959a1bdbcd967dabd3e93ab949a5861fc939
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2022-12-15 12:53:53 -0700

    complete addProperty refactor to insert into db

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -199,9 +199,72 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+
+  const {
+    owner_id,
+    title,
+    description,
+    thumbnail_photo_url,
+    cover_photo_url,
+    cost_per_night,
+    street,
+    city,
+    province,
+    post_code,
+    country,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms
+  } = property;
+
+
+  const query = {
+    text: `
+      INSERT INTO properties (
+        owner_id,
+        title,
+        description,
+        thumbnail_photo_url,
+        cover_photo_url,
+        cost_per_night,
+        parking_spaces,
+        number_of_bathrooms,
+        number_of_bedrooms,
+        country,
+        street,
+        city,
+        province,
+        post_code,
+        active
+      )
+      VALUES
+        ( $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14 )
+      RETURNING *;`,
+    values: [
+      owner_id,
+      title,
+      description,
+      thumbnail_photo_url,
+      cover_photo_url,
+      cost_per_night,
+      parking_spaces,
+      number_of_bathrooms,
+      number_of_bedrooms,
+      country,
+      street,
+      city,
+      province,
+      post_code
+    ],
+  };
+
+  return pool.query(query)
+  .then((result) => {
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
 };
 exports.addProperty = addProperty;
