0e24c75637ef1590965814a553ce9f831a9db2ba
Tai (he/him) <116750472+tai-de@users.noreply.github.com>
2022-12-15 08:58:29 -0700

    update login/registration queries to use the SQL db
    functions updated: getUserWithEmail, getUserWithId, addUser

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -18,16 +18,20 @@ const pool = new Pool({
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let user;
-  for (const userId in users) {
-    user = users[userId];
-    if (user.email.toLowerCase() === email.toLowerCase()) {
-      break;
-    } else {
-      user = null;
-    }
-  }
-  return Promise.resolve(user);
+
+  const query = {
+    text: `SELECT * FROM users WHERE email = $1;`,
+    values: [email],
+  };
+
+  return pool.query(query)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    }); 
+
 };
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -37,7 +41,20 @@ exports.getUserWithEmail = getUserWithEmail;
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+
+  const query = {
+    text: `SELECT * FROM users WHERE id = $1;`,
+    values: [id],
+  };
+
+  return pool.query(query)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    }); 
+
 };
 exports.getUserWithId = getUserWithId;
 
@@ -48,6 +65,21 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
+
+  const query = {
+    text: `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *;`,
+    values: [user.name, user.email, user.password],
+  };
+
+  return pool.query(query)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    }); 
+
+
   const userId = Object.keys(users).length + 1;
   user.id = userId;
   users[userId] = user;
